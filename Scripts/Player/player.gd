bfbad4731dbc3f272e1cfc1e6da3ae30ccc5ec83
Xaliz1 <Xavier.Byrne12@gmail.com>
2024-11-05 00:24:32 -0700

    Player movement & Placeholder assets
    I added Player movement and Place holder assets

---
+++ Scripts/Player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const movement_speed = 120
+var move_direction = Vector2.ZERO
+var desired_velocity = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	player_input()
+	apply_movement()
+
+
+func player_input():
+	move_direction = Vector2.ZERO
+	# Adjust move direction based on input actions.
+	# Horizontal movement: subtract 1 if 'Left' is pressed, add 1 if 'Right' is pressed.
+	move_direction.x = -int(Input.is_action_pressed("Left")) + int(Input.is_action_pressed("Right"))
+	# Vertical movement: subtract 1 if 'Up' is pressed, add 1 if 'Down' is pressed.
+	move_direction.y = -int(Input.is_action_pressed("Up")) + int(Input.is_action_pressed("Down"))
+	# Normalize the move direction to avoid faster diagonal movement and apply speed.
+	desired_velocity = move_direction.normalized() * movement_speed
+
+func apply_movement():
+	# Smoothly transition to desired velocity over time (lerp factor 0.1).
+	velocity = desired_velocity.lerp(desired_velocity, .1)
+	move_and_slide()
